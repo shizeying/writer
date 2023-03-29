b1d8abd4e52f2732c52d3f0a05dd01e4af4dc811
shizyeing <w741069229@163.com>
2023-03-29 11:29:18 +0800

    feature: support IDEA 231* version (IDEA 2023.1)

--- intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
+++ intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
@@ -2,13 +2,12 @@ package com.mintlify.document.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.startup.StartupActivity
 import com.mintlify.document.services.MyProjectService
 
-internal class MyProjectManagerListener : StartupActivity {
-    override fun runActivity(project: Project) {
+internal class MyProjectManagerListener : ProjectActivity {
+    override suspend fun execute(project: Project) {
         project.service<MyProjectService>()
     }
-
-
 }
