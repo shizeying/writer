3cac93c67703a7138380a8a67d588e9b258e98f6
shizyeing <w741069229@163.com>
2023-03-29 11:27:27 +0800

    feature: support IDEA 221* version (IDEA 2022.1)

--- intellij/build.gradle.kts
+++ intellij/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.8.20-RC2"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.13.2"
+    id("org.jetbrains.intellij") version "1.13.3"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
@@ -29,7 +29,7 @@ dependencies {
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.github.kittinunf.fuel:fuel-gson:2.3.1")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.beust:klaxon:5.5")
+    implementation("com.beust:klaxon:5.6")
     implementation(kotlin("stdlib-jdk8"))
 }
 
--- intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
+++ intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
@@ -2,12 +2,12 @@ package com.mintlify.document.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.ProjectActivity
 import com.mintlify.document.services.MyProjectService
 
-internal class MyProjectManagerListener : ProjectManagerListener {
-
-    override fun projectOpened(project: Project) {
-        project.service<MyProjectService>()
+internal class MyProjectManagerListener : ProjectActivity {
+    override suspend fun execute(project: Project) {
+         project.service<MyProjectService>()
     }
+
 }
