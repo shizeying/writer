18e6a5843030d3f83aee327ff377795988d6e32c
shizyeing <w741069229@163.com>
2023-03-29 11:35:50 +0800

    feature: support IDEA 222* version (IDEA 2022.2)

--- intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
+++ intellij/src/main/kotlin/com/mintlify/document/listeners/MyProjectManagerListener.kt
@@ -2,12 +2,12 @@ package com.mintlify.document.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManagerListener
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.startup.StartupActivity
 import com.mintlify.document.services.MyProjectService
 
-internal class MyProjectManagerListener : ProjectManagerListener {
-
-    override fun projectOpened(project: Project) {
+internal class MyProjectManagerListener : ProjectActivity {
+    override suspend fun execute(project: Project) {
         project.service<MyProjectService>()
     }
 }
